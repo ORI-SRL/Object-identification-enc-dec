219b12b338cb670adbc4067c4143bb4f65b9421c
shorty91195 <ollie@shorthose.org.uk>
2022-07-21 11:51:11 +0100

    Added BatchNorm+Dropout model just to see it's performance

--- utils/networks.py
+++ utils/networks.py
@@ -128,6 +128,52 @@ class TwoLayerWBatchNorm(nn.Module):
         return x_dec, x_enc
 
 
+class TwoLayerWDropoutWBatchNorm(nn.Module):
+
+    def __init__(self):
+        super(TwoLayerWDropoutWBatchNorm, self).__init__()
+
+        # Encoder
+        self.encoder_conv = nn.Sequential(
+            nn.Conv2d(1, 32, (1, 19), padding=0),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Dropout2d(.15),
+            nn.Conv2d(32, 16, (5, 1), padding=0),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.Dropout2d(.15),
+            nn.Conv2d(16, 8, (3, 1), padding=0),
+            nn.BatchNorm2d(8),
+            nn.ReLU(),
+            nn.Flatten()
+        )
+        # Embedding
+        x = self.encoder_conv(torch.zeros((1, 1, 10, 19)))
+        self.encoder_ff = nn.Sequential(nn.Linear(x.shape[-1], 100),
+                                        nn.Tanh(),
+                                        nn.Linear(100, 3))  # luca: squeeze the bottleneck to two neurons
+
+        # Decoder
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(1, 32, (10, 2)),
+            nn.Dropout2d(.2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 64, (1, 7)),
+            nn.Dropout2d(.2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 1, (1, 10)),
+        )
+
+    def forward(self, x):
+        x_enc = self.encoder_conv(x)
+        x_enc = self.encoder_ff(x_enc)  # here x_enc is the encoded two-dimensional vector and can be investigated
+
+        x_dec = self.decoder(x_enc.unsqueeze(1).unsqueeze(1))
+
+        return x_dec, x_enc
+
+
 class TwoLayerConv7Grasp(nn.Module):
 
     def __init__(self):
@@ -243,5 +289,3 @@ class TwoLayerConv3Grasp(nn.Module):
         x_dec = self.decoder(x_enc.unsqueeze(1).unsqueeze(1))
 
         return x_dec, x_enc
-
-
